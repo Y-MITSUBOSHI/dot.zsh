57aa84a6d10162668c878431827fe5f5f8ff61cb
Yuya MITSUBOSHI <yuya.mitsuboshi@p693.wn.oro.co.jp>
2016-07-01 21:49:03 +0900

    Added alias 'my_branch' to aliash.sh

--- conf.d/alias.sh
+++ conf.d/alias.sh
@@ -1,4 +1,4 @@
-# expand aliases before completing
+#expand aliases before completing
 setopt complete_aliases # aliased ls needs if file/dir completions work
 
 case "${OSTYPE}" in
@@ -36,3 +36,22 @@ fi
 # if [ "${PAGER}" != "less" ]; then
 #   alias less=${PAGER}
 # fi
+#alias for git
+
+alias gad="git add"
+alias gcm="git commit"
+alias gpush="git push"
+alias gpull="git pull"
+alias gchk="git checkout"
+alias gchkb="git checkout -b"
+alias gs="git status"
+alias gsv="git status -v"
+alias gl="git log"
+alias gbranch="git branch"
+alias my_branch="git branch | grep '*' | sed 's;*;;g'"
+
+export PAGER=/usr/local/bin/vimpager
+alias less=$PAGER
+alias zless=$PAGER
+
+export SVN_EDITOR=vim
